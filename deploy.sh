cfa69b786d5f77a74a41613122f1364848f3e277
fant1kua <fant1kua@users.noreply.github.com>
2020-06-04 00:28:52 +0300

    Bump version

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ cat > scripting/include/uac_version.inc <<EOT
 #define UAC_MAJOR_VERSION       0
 #define UAC_MINOR_VERSION       1
 #define UAC_MAINTENANCE_VERSION $version
-#define UAC_VERSION_STR         "0.1.$version"
+#define UAC_VERSION_STR         "0.1.$version-alpha"
 EOT
 
 zip -9 -r -q --exclude=".git/*" --exclude=".gitignore" --exclude=".gitkeep" --exclude=".travis.yml" --exclude="README.md" --exclude="deploy.sh" user-access-control.zip .
